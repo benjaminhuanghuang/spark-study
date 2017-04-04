f66a40110c3a2944e4ab5bfa2e9dd1bc468fdae2
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-04 01:02:27 -0700

    update

--- simple_app/project/Dependencies.scala
+++ simple_app/project/Dependencies.scala
@@ -3,10 +3,24 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
   
+  val resolutionRepos = Seq(
+    "Akka Repository" at "http://repo.akka.io/releases/",
+    "Spray Repository" at "http://repo.spray.cc/"
+  )
+  
+  object V {
+    val spark     = "1.6.0"
+    val specs2    = "1.13"
+    val guava     = "11.0.1"
+    // Add versions for your additional libraries here...
+  }
+
   object Libraries {
     val sparkCore    = "org.apache.spark"           %% "spark-core"            % V.spark        % "provided"
     val sparkMllib   = "org.apache.spark"           %% "spark-mllib"           % V.spark        % "provided"
     val sparkSql     = "org.apache.spark"           %% "spark-sql"             % V.spark        % "provided"
+    
+    
     // Add additional libraries from mvnrepository.com (SBT syntax) here...
 
     // Scala (test only)
